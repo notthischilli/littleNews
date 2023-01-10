c60ef90fc794bb794f65214b8eb988fe8c6de6d6
notthischilli <notthischilli@gmail.com>
2023-01-10 13:44:36 +0100

    origin change and route redirected

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+const cors = require('cors')({origin: true});
 require('dotenv').config();
 
 const mongoose = require('mongoose');
@@ -11,19 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-var allowedOrigins = ['https://littlenews.onrender.com', 'littlenews.onrender.com']
-
-app.use(cors({
-  origin: function(origin, callback){    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }    return callback(null, true);
-  }
-}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -36,6 +23,9 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log(`connection to DB established`)})
 .catch(err=>{console.log(err)})
 
+app.get('/', (req, res)=>{
+    res.redirect('/news')
+})
 app.use('/news', indexRouter);
 app.use('/users', usersRouter);
 
